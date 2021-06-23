04b0d4dd487aca28cdc50b600c14b9b02b42e92f
韦武浚 <560391@gree.com.cn>
2021-06-23 16:00:16 +0800

    (fit)添加获取在passThroughParams中的多个params

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,39 @@
 </template>
 
 <script>
-import commonUtil from './utils/index';
-import { isGreeMiniProgram } from './utils/index'
+import commonUtil from "./utils/index";
+import { isGreeMiniProgram, getMac, getParams } from "./utils/index";
 console.log(commonUtil);
-console.log(isGreeMiniProgram)
-console.log(isGreeMiniProgram('https://127.0.0.1/url?passThroughParams=greeapp&test=1'))
+console.log(isGreeMiniProgram);
+console.log(
+  isGreeMiniProgram("https://127.0.0.1/url?passThroughParams=greeapp&test=1")
+);
+console.log(
+  isGreeMiniProgram(
+    "https://iot.cloud.tencent.com/scf/h5panel/?%21debug=0&deviceId=KNR0SMK6SQ%2F294906&familyId=&passthrough=greeapp%26mac%3D12345678&ticket=t_08dba794423947209999324591634d68&uin=greetest",
+    "passthrough"
+  )
+);
+console.log(
+  getParams(
+    "https://iot.cloud.tencent.com/scf/h5panel/?%21debug=0&deviceId=KNR0SMK6SQ%2F294906&familyId=&passthrough=%5Borigin%3Dgreeapp%26mac%3D123456%5D&ticket=t_82ad69d3cd6043c49deb50ca5bbc47d2&uin=greetest",
+    "passthrough"
+  )
+);
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -30,7 +45,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,32 +1,52 @@
-export function isGreeMiniProgram(url, key = 'passThroughParams', value = 'greeapp') {
+export function isGreeMiniProgram(
+  url,
+  key = "passThroughParams",
+  value = "greeapp"
+) {
   if (url === undefined) {
-    if (typeof window !== 'undefined' && window && window.location && window.location.href) {
-      url = window.location.href
+    if (
+      typeof window !== "undefined" &&
+      window &&
+      window.location &&
+      window.location.href
+    ) {
+      url = window.location.href;
     } else {
-      throw new Error(`'isGreeMiniProgram' method param 'url' must exist and is type of 'string'`)
+      throw new Error(
+        `'isGreeMiniProgram' method param 'url' must exist and is type of 'string'`
+      );
     }
   }
-  if (typeof url !== 'string') {
-    throw new Error(`'isGreeMiniProgram' method param 'url' must be of type 'string'`)
+  if (typeof url !== "string") {
+    throw new Error(
+      `'isGreeMiniProgram' method param 'url' must be of type 'string'`
+    );
   }
-  if (typeof key !== 'string') {
-    throw new Error(`'isGreeMiniProgram' method param 'key' must be of type 'string'`)
+  if (typeof key !== "string") {
+    throw new Error(
+      `'isGreeMiniProgram' method param 'key' must be of type 'string'`
+    );
   }
-  if (typeof value !== 'string') {
-    throw new Error(`'isGreeMiniProgram' method param 'value' must be of type 'string'`)
+  if (typeof value !== "string") {
+    throw new Error(
+      `'isGreeMiniProgram' method param 'value' must be of type 'string'`
+    );
   }
   if (!url) {
     return false;
   }
-  const queryAndHash = url.split('?')[1];
+  url = decodeURIComponent(url);
+  const queryAndHash = url.split("?")[1];
   if (!queryAndHash) {
     return false;
   }
-  const query = queryAndHash.split('#')[0];
+  const query = queryAndHash.split("#")[0];
   if (!query) {
     return false;
   }
-  for(const [queryKey, queryVal] of query.split('&').map(kv => kv.split('='))) {
+  for (const [queryKey, queryVal] of query
+    .split("&")
+    .map(kv => kv.split("="))) {
     if (queryKey === key) {
       if (queryVal === value) {
         return true;
@@ -38,6 +58,73 @@ export function isGreeMiniProgram(url, key = 'passThroughParams', value = 'greea
   return false;
 }
 
-export default {
-  isGreeMiniProgram
+export function getParams(url, key = "passThroughParams") {
+  // 如果没传url
+  if (url === undefined) {
+    if (
+      typeof window !== "undefined" &&
+      window &&
+      window.location &&
+      window.location.href
+    ) {
+      // 则默认使用window.location.href
+      url = window.location.href;
+    } else {
+      // 否则抛出异常
+      throw new Error(
+        `'isGreeMiniProgram' method param 'url' must exist and is type of 'string'`
+      );
+    }
+  }
+  // 如果给了url，就判断是否为字符串
+  if (typeof url !== "string") {
+    throw new Error(
+      `'isGreeMiniProgram' method param 'url' must be of type 'string'`
+    );
+  }
+  if (typeof key !== "string") {
+    throw new Error(
+      `'isGreeMiniProgram' method param 'key' must be of type 'string'`
+    );
+  }
+  const queryAndHash = url.split("?")[1];
+  if (!queryAndHash) {
+    return null;
+  }
+
+  const query = queryAndHash.split("#")[0];
+  if (!query) {
+    return null;
+  }
+  for (const [queryKey, queryVal] of query
+    .split("&")
+    .map(kv => kv.split("="))) {
+    console.log("wwj queryKey, queryVal", queryKey, queryVal);
+    if (queryKey === key) {
+      let pal = decodeURIComponent(queryVal);
+      return parseScanCodeUrl(pal.substr(1, pal.length - 2));
+    }
+  }
+
+  return url;
 }
+
+function parseScanCodeUrl(url) {
+  // 创建目标对象
+  let tempUrlResult = {};
+  // 截取数据字符串,将数据字符串表现为数组
+  let tempUrlData = url.split("&");
+  for (let i = 0; i < tempUrlData.length; i++) {
+    // 数据属性与数值分离
+    let item = tempUrlData[i].split("=");
+    // 使用属性括号进行属性赋值
+    tempUrlResult[item[0]] = item[1];
+  }
+  console.log(tempUrlResult);
+  return tempUrlResult;
+}
+
+export default {
+  isGreeMiniProgram,
+  getParams
+};
